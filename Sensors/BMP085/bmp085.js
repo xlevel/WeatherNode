2916a3b83f77c696c8aa15e8655a2b8b5a11c4b6
xlevel <jim@xlevel.org.uk>
2017-12-08 17:21:29 +0000

    Updated the BMP085 sensor for app launch

---
+++ Sensors/BMP085/bmp085.js
@@ -0,0 +1,24 @@
+const SensorLib = require('bmp085');
+
+class BMP085Sensor {
+  constructor(config) {
+    this.config = config;
+  }
+
+  read(callback) {
+    const sensor = new SensorLib();
+    sensor.read((sensorData) => {
+      const data = {
+        id: this.config.id,
+        readings: [
+          { type: 't', value: sensorData.temperature.toFixed(2) },
+          { type: 'p', value: sensorData.pressure.toFixed(2) },
+        ],
+      };
+      callback(data);
+    });
+  }
+}
+
+module.exports = BMP085Sensor;
+
