c32feaa80b75bb21ec8388e731ff23e5a6d94de1
xlevel <jim@xlevel.org.uk>
2017-12-08 16:53:35 +0000

    Updated the DS18b20 sensor for app relaunch

---
+++ Sensors/DS18B20/ds18b20-sensor.js
@@ -0,0 +1,22 @@
+const sensorLib = require('ds18b20');
+
+class DS18B20Sensor {
+  constructor(config) {
+    this.config = config;
+  }
+
+  read(callback) {
+    const reading = sensorLib.temperatureSync(this.config.settings.id);
+
+    const data = {
+      id: this.config.id,
+      readings: [
+        { type: 't', value: reading.toFixed(2) },
+      ],
+    };
+
+    callback(data);
+  }
+}
+
+module.exports = DS18B20Sensor;
