63ce1c3cc284828f5748ec5b9048de6b8fbd63ef
Jim Hurst <xlevel@users.noreply.github.com>
2020-06-21 15:25:54 +0100

    Fixed BMP085 payload

--- Sensors/BMP085/bmp085.js
+++ Sensors/BMP085/bmp085.js
@@ -1,18 +1,17 @@
 const bmp180 = require('bmp180-sensor');
 
 async function read(config) {
-  const sensor = await bmp180(config.settings.address || 0x77, config.settings.mode || 1);
+  const sensor = await bmp180({ address: 0x77, mode: 1 });
 
   const reading = await sensor.read();
 
   sensor.close();
 
-  console.log(reading);
   return {
     id: config.id,
     readings: [
-      // { type: 't', value: reading.temperature.toFixed(2) },
-      // { type: 'h', value: reading.humidity.toFixed(2) },
+      { type: 't', value: reading.temperature.toFixed(2) },
+      { type: 'p', value: reading.pressure.toFixed(2) },
     ],
   };
 }
